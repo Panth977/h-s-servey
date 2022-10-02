9daa58be2e937f7a0989ceca1447ee4fa1e241ff
Panth <hybrid977@gmail.com>
2022-10-02 12:26:24 +0530

    fix

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,10 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
-/// <reference types="@sveltejs/adapter-cloudflare-workers" />
 declare namespace App {
 	interface Platform {
-		env?: {
-			YOUR_KV_NAMESPACE: KVNamespace;
-			YOUR_DURABLE_OBJECT_NAMESPACE: DurableObjectNamespace;
+		env: {
+			COUNTER: DurableObjectNamespace;
 		};
 		context: {
 			waitUntil(promise: Promise<any>): void;

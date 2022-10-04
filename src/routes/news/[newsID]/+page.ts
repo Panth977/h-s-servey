9ba68544c50891e23c5f1a8033f7ac0776e426da
Panth <hybrid977@gmail.com>
2022-10-04 12:32:32 +0530

    ssr added

---
+++ src/routes/news/[newsID]/+page.ts
@@ -0,0 +1,16 @@
+import { NewsColl, type News } from '$lib/firebase/db';
+import { doc, getDoc } from 'firebase/firestore';
+import type { PageLoad } from './$types';
+import { error } from '@sveltejs/kit';
+
+export const load: PageLoad<News> = async ({ params }) => {
+	try {
+		const res = await getDoc(doc(NewsColl, params.newsID));
+		const news = res.data();
+		if (!news) throw error(404, 'Not found');
+		return news;
+	} catch (e) {
+		throw error(404, 'Not found');
+	}
+};
+export const hydrate = true;

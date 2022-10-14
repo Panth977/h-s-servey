d0139ae3e4c4d5a850486fd539d0e44310d0dac8
Panth <hybrid977@gmail.com>
2022-10-15 03:14:48 +0530

    dynimic event

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,14 +5,14 @@
 
 <Header tralingLogo />
 {#each $config.sortedEvent as event}
-	{#if true || !event.hidden}
+	{#if !event.hidden}
 		<a href="/event/{event.id}" class="page-padding mt-5 block">
-			<div class="bg-base2 border rounded-md shadow-xl">
+			<div class="bg-base2 border rounded-lg shadow-xl">
 				<h2 class="text-xl mx-3 items-end font-bold py-2 flex justify-between">
 					{event.title}
 					<span class="text-xs font-medium">{event.displayDate}</span>
 				</h2>
-				<img src={event.poster} alt={event.title} class="w-full rounded-b-md block object-cover" />
+				<img src={event.poster} alt={event.title} class="w-full rounded-b-lg block object-cover" />
 			</div>
 		</a>
 	{/if}
